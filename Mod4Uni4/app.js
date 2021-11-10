ee1c61e8e2a082e6b4a99c453647bc72805f61af
Brunella Bernat <brunellabernat23@gmail.com>
2021-11-10 16:24:07 -0300

    Tarea

--- Mod4Uni4/app.js
+++ Mod4Uni4/app.js
@@ -5,12 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var nosotrosRouter = require('./routes/nosotros');
 var contactosRouter = require('./routes/contactos');
 var mapaRouter = require('./routes/mapa');
 var loginRouter = require('./routes/admin/login')
+var novedadesRouter = require('./routes/admin/novedades')
+
 
 var app = express();
 
@@ -24,11 +27,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'abcdefghijklmn1234567',
+  resave:false,
+  saveUninitialized: true
+}));
+
+secured = async(req,res,next) => {
+  try{
+    console.log(req.session.id_usuario);
+    if(req.session.id_usuario){
+      next();
+    }else{
+      res.redirect('login');
+    }
+  }catch(error){
+    console.log(error);
+  }
+}
+
 app.use('/', indexRouter);
 app.use('/nosotros', nosotrosRouter);
 app.use('/contactos', contactosRouter);
 app.use('/mapa', mapaRouter);
 app.use('/admin/login', loginRouter)
+app.use('/admin/novedades',secured, novedadesRouter)
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Mod4Uni4/routes/admin/login.js
+++ Mod4Uni4/routes/admin/login.js
@@ -1,35 +1,45 @@
 var express = require('express');
 var router = express.Router();
-var usuariosModel =require('./../../models/usuariosModel');
+var ususariosModel = require('./../../models/usuariosModel');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('admin/login', {
-      layout:'admin/layout'
+    layout: 'admin/layout'
+  });
+});
+
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.render('admin/login', {
+    layout: 'admin/layout'
   });
 });
 
 router.post('/', async (req, res, next) => {
-  try{
+  try {
     var usuario = req.body.usuario;
     var password = req.body.password;
 
-    console.log(req.body);
 
-    var data = await usuariosModel.getUserAndPassword(usuario, password);
+    var data = await ususariosModel.getUserAndPassword(usuario, password);
 
-    if(data != undefined) {
+    if (data != undefined) {
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario;
       res.redirect('/admin/novedades');
     } else {
       res.render('admin/login', {
-      layout: 'admin/layout',
-      error: true
-    });
+        layout: 'admin/layout',
+        error: true
+      });
     }
+      
+  }catch (error)
 
-  }catch(error) {
-    console.log(error);
-  }
-})
+
+{}})
 
 module.exports = router;
+
+
+
